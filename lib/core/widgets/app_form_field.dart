a4ee1f9c7d47d90a1d06f4ceee4d16d51de0f157
Agroudi <Abdelrahman2004amr@gmail.com>
2026-03-04 14:54:10 +0200

    To be continued

--- lib/core/widgets/app_form_field.dart
+++ lib/core/widgets/app_form_field.dart
@@ -2,6 +2,7 @@ import 'package:bookia_app/core/theme/colors.dart';
 import 'package:bookia_app/core/theme/text_style.dart';
 import 'package:bookia_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -22,7 +23,9 @@ class _AppFormFieldState extends State<AppFormField> {
   {
     return TextFormField(
       autocorrect: true,
-      keyboardType: widget.hintTxt == "Enter your email" ? TextInputType.emailAddress : widget.hintTxt == "Enter your password" ? TextInputType.visiblePassword : TextInputType.text,
+      keyboardType: widget.hintTxt == "Enter your email" ? TextInputType.emailAddress
+          : widget.hintTxt == "Enter your password" ? TextInputType.visiblePassword
+          : TextInputType.text,
       obscureText: widget.obsecureText,
       cursorColor: AppColors.Primary,
       onTapOutside: (event) => FocusScope.of(context).unfocus(),
@@ -36,7 +39,7 @@ class _AppFormFieldState extends State<AppFormField> {
         suffixIcon: widget.hintTxt == "Enter your password" ? Padding(
           padding: const EdgeInsets.all(12.0),
           child: InkWell(onTap: () => setState(() => widget.obsecureText = !widget.obsecureText),child: SvgPicture.asset(Assets.icons.eye)),) : null,
-        hintStyle: AppTextStyle.txtStyle.copyWith(color: AppColors.Black.withOpacity(0.6))
+        hintStyle: AppTextStyle.txtStyle.copyWith(color: AppColors.Black.withOpacity(0.6), fontSize: 15.sp)
       )
     );
   }
